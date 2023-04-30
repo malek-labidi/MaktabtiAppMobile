46685864886ce0e51f43f4ae4e6d7021226caec7
malek-labidi <labidimalek@esprit.tn>
2023-04-30 15:17:32 +0100

    add statics file (base url)

---
+++ src/com/genesisteam/maktabti/utilities/Statics.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.genesisteam.maktabti.utilities;
+
+/**
+ *
+ * @author admin
+ */
+public class Statics {
+    public static String BASE_URL = "https://127.0.0.1:8000";
+}
