c82f4430b625d5c78fb8605cb8d1e8854cc9586c
hachaniwassim <wassimhach16@gmail.com>
2023-05-02 00:34:13 +0100

    template login and register but still dosn't work

--- src/com/genesisteam/maktabti/gui/Login.java
+++ src/com/genesisteam/maktabti/gui/Login.java
@@ -5,10 +5,80 @@
  */
 package com.genesisteam.maktabti.gui;
 
+import com.codename1.components.FloatingHint;
+import com.codename1.ui.Button;
+import com.codename1.ui.Container;
+import com.codename1.ui.Display;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.genesisteam.maktabti.services.UtilisateurService;
+
 /**
  *
- * @author wassi
+ * @author wassim
  */
-public class Login {
+public class Login extends BaseForm{
+  public Login() {
+        super(new BorderLayout());
+        
+        if(!Display.getInstance().isTablet()) {
+            BorderLayout bl = (BorderLayout)getLayout();
+            bl.defineLandscapeSwap(BorderLayout.NORTH, BorderLayout.EAST);
+            bl.defineLandscapeSwap(BorderLayout.SOUTH, BorderLayout.CENTER);
+        }
+        getTitleArea().setUIID("Container");
+        setUIID("SignUp");
+        
+//        add(BorderLayout.NORTH, new Label(res.getImage("Logo.png"), "LogoLabel"));
+        
+        TextField username = new TextField("", "email", 20, TextField.ANY);
+        TextField password = new TextField("", "Password", 20, TextField.PASSWORD);
+        username.setSingleLineTextArea(false);
+        password.setSingleLineTextArea(false);
+        Button signIn = new Button("Sign In");
+        Button signUp = new Button("Sign Up");
+        
+        //mp oublié
+        Button  mp = new Button("oublier mot de passe?","CenterLabel");
+        
+        
+        signUp.addActionListener(e -> new Register().show());
+        signUp.setUIID("Link");
+        Label doneHaveAnAccount = new Label("Vous n'avez aucune compte?");
+        
+        
+        
+        
+        
+        
+        Container content = BoxLayout.encloseY(
+                new FloatingHint(username),
+                createLineSeparator(),
+                new FloatingHint(password),
+                createLineSeparator(),
+                signIn,
+                FlowLayout.encloseCenter(doneHaveAnAccount, signUp),mp
+        );
+        content.setScrollableY(true);
+        add(BorderLayout.SOUTH, content);
+        signIn.requestFocus();
+        
+        signIn.addActionListener(e -> 
+        {
+             UtilisateurService.getInstance().signin(username, password, null);
+
+           
+        });
+        
+        
+        
+        //Mp oublie event
+        
+
+        
+    }
     
 }
