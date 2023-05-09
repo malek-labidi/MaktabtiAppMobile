8a3850b06a680fd3f727c4ddc09622c484ecd0de
gaaloul2000 <aziz.gaaloul@istic.ucar.tn>
2023-05-09 16:20:16 +0100

    display fidelité , consommation web service

--- src/com/genesisteam/maktabti/services/FideliteService.java
+++ src/com/genesisteam/maktabti/services/FideliteService.java
@@ -4,9 +4,23 @@
  */
 package com.genesisteam.maktabti.services;
 
+import com.codename1.io.CharArrayReader;
 import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.l10n.ParseException;
+import com.codename1.l10n.SimpleDateFormat;
+import com.codename1.ui.events.ActionListener;
+import com.genesisteam.maktabti.entities.Competition;
 import com.genesisteam.maktabti.entities.Fidelite;
+import com.genesisteam.maktabti.entities.Type;
+import com.genesisteam.maktabti.utilities.Statics;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -35,4 +49,64 @@ public class FideliteService {
     }
     
     
+   public Fidelite getFidelite(int id) {
+        req = new ConnectionRequest();
+
+        // Build the API URL with the competition ID
+        String fetchURL = Statics.BASE_URL + "/fidelitee/get/" + id;
+
+        //2
+        req.setUrl(fetchURL);
+
+        //3
+        req.setPost(false);
+
+        //4
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                try {
+                    fidelite = fromJson(new String(req.getResponseData(), "UTF-8"));
+                } catch (UnsupportedEncodingException ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+                req.removeResponseListener(this);
+            }
+        });
+
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return fidelite;
+    }
+
+     public Fidelite fromJson(String jsonText) {
+        JSONParser jp = new JSONParser();
+        fidelite = new Fidelite();
+
+        try {
+
+            //2
+            Map<String, Object> FidelitesListJSON = jp.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+
+            //3
+            List<Map<String, Object>> list = (List<Map<String, Object>>) FidelitesListJSON.get("root");
+            for (Map<String, Object> item : list) {
+                float idfidelite = Float.parseFloat(item.get("idFidelite").toString());
+                fidelite.setId_fidelite((int)idfidelite );
+                float total = Float.parseFloat(item.get("totalAchat").toString());
+                fidelite.setTotal_achat((int)total );
+                fidelite.setType(Type.valueOf((String) item.get("type")));
+                
+                float idclient = Float.parseFloat(item.get("idClient").toString());
+                fidelite.setId_client((int)idclient );
+                
+               
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return fidelite;
+    }
+
 }
--- src/com/genesisteam/maktabti/services/OffreService.java
+++ src/com/genesisteam/maktabti/services/OffreService.java
@@ -4,9 +4,19 @@
  */
 package com.genesisteam.maktabti.services;
 
+import com.codename1.io.CharArrayReader;
 import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import com.genesisteam.maktabti.entities.Evenement;
 import com.genesisteam.maktabti.entities.Offre;
+import com.genesisteam.maktabti.utilities.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -35,4 +45,74 @@ public class OffreService {
         return instance;
     }
     
+    
+    
+    
+    
+     public List<Offre> fetchOffres() {
+
+        req = new ConnectionRequest();
+
+        //1
+        String fetchURL = Statics.BASE_URL + "/offre/get";
+
+        //2
+        req.setUrl(fetchURL);
+
+        //3
+        req.setPost(false);
+
+        //4
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                offres = parseOffres(new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return offres;
+    } 
+    
+       
+    
+     public List<Offre> parseOffres(String jsonText) {
+
+        //var
+        offres = new ArrayList<>();
+
+        //DO
+        //1
+        JSONParser jp = new JSONParser();
+
+        try {
+
+            //2
+            Map<String, Object> OffresListJSON = jp.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+
+            //3
+            List<Map<String, Object>> list = (List<Map<String, Object>>) OffresListJSON.get("root");
+
+            //4
+            for (Map<String, Object> item : list) {
+
+                Offre o = new Offre();
+                float idoffre = Float.parseFloat(item.get("idOffre").toString());
+                o.setId_offre((int)idoffre ); 
+                float idlivre = Float.parseFloat(item.get("idLivre").toString());
+                o.setId_livre((int)idlivre );
+                float prixsolde = Float.parseFloat(item.get("prixSolde").toString());
+                o.setId_livre((int)prixsolde );
+
+                offres.add(o);
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return offres;
+    }
+
 }
